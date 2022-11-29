b4a2c5be742571480f951e65208c27b31f5e90fb
sammyHong <samee1@naver.com>
2022-11-29 13:33:15 +0900

    영화 상세정보 가져오기

--- src/store/movie.js
+++ src/store/movie.js
@@ -9,6 +9,7 @@ export default {
     movies: [],
     message: "Search for the movie title!",
     loading: false,
+    theMovie: {},
   }),
   //computed!
   getters: {},
@@ -80,6 +81,30 @@ export default {
         });
       }
     },
+    async searchMovieWithId({ state, commit }, payload) {
+      if (state.loading) return;
+
+      commit("updateState", {
+        theMovie: {},
+        loading: true,
+      });
+
+      try {
+        const res = await _fetchMovie(payload);
+        commit("updateState", {
+          theMovie: res.data,
+        });
+        console.log(res);
+      } catch (error) {
+        commit("updateState", {
+          theMovie: {},
+        });
+      } finally {
+        commit("updateState", {
+          loading: false,
+        });
+      }
+    },
   },
 };
 
@@ -87,7 +112,9 @@ export default {
 function _fetchMovie(payload) {
   const { title, type, year, page } = payload;
   const OMDB_API_KEY = "7035c60c";
-  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
   // const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}`;
 
   return new Promise((resolve, reject) => {
